149ddea9675032ee3a9821517aff6f540973c41f
Ali <ali@brandver.se>
2022-02-21 16:11:48 +0500

    Added theme

--- src/app/App.js
+++ src/app/App.js
@@ -1,9 +1,14 @@
-import SideBar from "./SideBar"
+import { ThemeProvider } from "@mui/material/styles"
+import theme from "./theme"
+
+import BottomAppBar from "./BottomBar"
+import { Grid } from "@mui/material"
 
 const App = () => (
-  <>
-    <SideBar />
-  </>
+  <Grid>
+    <ThemeProvider theme={theme}>
+      <BottomAppBar />
+    </ThemeProvider>
+  </Grid>
 )
-
 export default App
